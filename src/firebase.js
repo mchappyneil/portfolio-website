d5ef31b5d9f86184f0ec9512c941381aa0b217e2
Neil Mehta <neil.mehta0201@gmail.com>
2025-03-28 11:59:59 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { getFirestore } from 'firebase/firestore/lite'
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCh3BMS9wYLdEVuf3h-SrELGpDapIf9m2Q",
-  authDomain: "react-portfolio-site-dashboard.firebaseapp.com",
-  projectId: "react-portfolio-site-dashboard",
-  storageBucket: "react-portfolio-site-dashboard.appspot.com",
-  messagingSenderId: "744205106010",
-  appId: "1:744205106010:web:434eda354e37fa2762d163"
+  apiKey: "hidden",
+  authDomain: "hidden",
+  projectId: "hidden",
+  storageBucket: "hidden",
+  messagingSenderId: "hidden",
+  appId: "hidden"
 };
 
 const app = initializeApp(firebaseConfig);
